755a4b163714e24532d736f8fb59a1cb66523e44
nikitabalo <fiolmail@list.ru>
2022-10-27 23:34:36 +0300

    lesson_8.1

--- srx.sql
+++ srx.sql
@@ -5,7 +5,17 @@ select distinct a.address, a1.address from address a inner join address a1 on a.
 use nikitabd;
 select p.fullname, r.review_message from reviews r inner join users u on u.id = r.id_user inner join profile p on p.id_user = u.id where p.fullname like '%n%';
 select m.id_movie, count(ra.id_movie) as 'Count of views' from movies m inner join recent_actions ra on m.id_movie = ra.id_movie group by m.id_movie;
-select u.id from subscription s 
-inner join users_has_subscription uhs on s.id_subscription = uhs.subscription_id 
-inner join users u on u.id = uhs.user_id 
-where s.id_subscription = (select s.id_subscription where datediff(adddate(date(s.date_of_activate), s.duration),date('2022-10-27')) > 0);#не работает
+select
+	u.id
+from
+	subscription s
+inner join users_has_subscription uhs on
+	s.id_subscription = uhs.subscription_id
+inner join users u on
+	u.id = uhs.user_id
+where
+	s.id_subscription = (
+	select
+		s.id_subscription
+	where
+		datediff(adddate(date(s.date_of_activate), s.duration), date('2022-10-27')) > 0);#не работает
